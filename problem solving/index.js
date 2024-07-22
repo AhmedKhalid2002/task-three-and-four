0b33b9784d1c38e96b0d3b4c1283db778f44c1e2
AhmedKhalid2002 <kmalahmdkhald927@gmail.com>
2024-07-22 19:39:18 +0300

    first commit

---
+++ problem solving/index.js
@@ -0,0 +1,50 @@
+// practice one
+// let nums=[10,20,30,50,40,60];
+// function summition(arr){
+//     let sum=0;
+//     for(let i=0;i<arr.length;i++){
+//         sum+=arr[i];
+//     }
+//     return sum;
+// }
+// const result=summition(nums)
+// console.log(result);
+
+// practice Two
+// let numbers=[10,9,1];
+// function maxNumbers(arr){
+//     let maxNum=arr[0];
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i] > maxNum){
+//             maxNum=arr[i];
+//         }
+//     }
+//     return maxNum;
+// }
+// let result=maxNumbers(numbers)
+// console.log(result);
+
+// practice three
+// let studentName="Ahmed khalid kamal";
+// function reverseString(str){
+//     let arr=str.split("");
+//     arr.reverse();
+
+//     let revString=arr.join("");
+//     return revString;
+// }
+
+// let result =reverseString(studentName);
+// console.log(result);
+
+// practice Four
+// function deleteDuplicate(arr){
+
+//     let deleteDup =new Set(arr);
+//     let returnArr=Array.from(deleteDup)
+
+//     return returnArr;
+// }
+// let numbers = [2, 5, 7, 8, 3, 8, 30, 2];
+// let result = deleteDuplicate(numbers);
+// console.log(result);
